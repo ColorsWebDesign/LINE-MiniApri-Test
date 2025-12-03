739d70ca03d487937cb75c338b680ba36b4c89d2
ColorsWebDesign <fujiwara@recap.jp>
2025-12-04 05:33:58 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
-import { page1 } from './components/page1'
-import { page2 } from './components/page2'
-import { page3 } from './components/page3'
 
 function App() {
   const [name, setName] = useState("");
@@ -29,7 +26,7 @@ function App() {
       </div>
       {name && <p class="name">こんにちは、<br /><span>{name}</span>さん</p>}
       <p>同窓会ミニアプリテスト版です</p>
-<Router>
+
       <div class="nav">
         <div class="clm01"><img src="https://colors.cc/line_test/intro001.jpg" alt="お知らせ" /></div>
         <div class="clm03">
@@ -40,14 +37,9 @@ function App() {
           <div><img src="https://colors.cc/line_test/nav005.jpg" alt="お問い合わせ" /></div>
           <div><img src="https://colors.cc/line_test/nav006.jpg" alt="設定" /></div>
 
-          <Routes>
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
-          <Route path="/page3" element={<Page3 />} />
-        </Routes>
         </div>
       </div>
-</Router>
+
     </div>
   );
 }
