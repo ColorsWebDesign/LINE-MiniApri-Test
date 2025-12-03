650976c256e6f1fe1dba162edfff81e44ff61e5f
ColorsWebDesign <fujiwara@recap.jp>
2025-12-04 05:45:12 +0900

    update Link

--- src/App.jsx
+++ src/App.jsx
@@ -30,19 +30,25 @@ function App() {
       {name && <p class="name">こんにちは、<br /><span>{name}</span>さん</p>}
       <p>同窓会ミニアプリテスト版です</p>
 
+<Router>
       <div class="nav">
         <div class="clm01"><img src="https://colors.cc/line_test/intro001.jpg" alt="お知らせ" /></div>
         <div class="clm03">
-          <div><img src="https://colors.cc/line_test/nav001.jpg" alt="お知らせ" /></div>
-          <div><img src="https://colors.cc/line_test/nav002.jpg" alt="プロフィール" /></div>
-          <div><img src="https://colors.cc/line_test/nav003.jpg" alt="同窓会" /></div>
+          <div><Link to="/page1"><img src="https://colors.cc/line_test/nav001.jpg" alt="お知らせ" /></Link></div>
+          <div><Link to="/page2"><img src="https://colors.cc/line_test/nav002.jpg" alt="プロフィール" /></Link></div>
+          <div><Link to="/page3"><img src="https://colors.cc/line_test/nav003.jpg" alt="同窓会" /></Link></div>
           <div><img src="https://colors.cc/line_test/nav004.jpg" alt="コミュニティ" /></div>
           <div><img src="https://colors.cc/line_test/nav005.jpg" alt="お問い合わせ" /></div>
           <div><img src="https://colors.cc/line_test/nav006.jpg" alt="設定" /></div>
 
+          <Routes>
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+          <Route path="/page3" element={<Page3 />} />
+        </Routes>
         </div>
       </div>
-
+</Router>
     </div>
   );
 }
