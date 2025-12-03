fcd7ad59273131a4d0986bcdf9e3c5d4c2afcae4
ColorsWebDesign <fujiwara@recap.jp>
2025-12-04 05:39:35 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,26 @@ function App() {
   
   return (
     <div className="App">
-      {name && <p>こんにちは、{name}さん</p>}
+      <div class="top">
+      <div class="right">未認証</div>
+      <div class="left">登録年月日 2025/10/10</div>
+      </div>
+      {name && <p class="name">こんにちは、<br /><span>{name}</span>さん</p>}
+      <p>同窓会ミニアプリテスト版です</p>
+
+      <div class="nav">
+        <div class="clm01"><img src="https://colors.cc/line_test/intro001.jpg" alt="お知らせ" /></div>
+        <div class="clm03">
+          <div><img src="https://colors.cc/line_test/nav001.jpg" alt="お知らせ" /></div>
+          <div><img src="https://colors.cc/line_test/nav002.jpg" alt="プロフィール" /></div>
+          <div><img src="https://colors.cc/line_test/nav003.jpg" alt="同窓会" /></div>
+          <div><img src="https://colors.cc/line_test/nav004.jpg" alt="コミュニティ" /></div>
+          <div><img src="https://colors.cc/line_test/nav005.jpg" alt="お問い合わせ" /></div>
+          <div><img src="https://colors.cc/line_test/nav006.jpg" alt="設定" /></div>
+
+        </div>
+      </div>
+
     </div>
   );
 }
