791f323fb8c3411aabdb4b8843ec4d614ec1386c
ColorsWebDesign <fujiwara@recap.jp>
2025-12-04 05:50:49 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
-import { page1 } from './components/page1'
-import { page2 } from './components/page2'
-import { page3 } from './components/page3'
+import { page1 } from './components/page1';
+import { page2 } from './components/page2';
+import { page3 } from './components/page3';
 
 function App() {
   const [name, setName] = useState("");
@@ -34,9 +34,9 @@ function App() {
       <div class="nav">
         <div class="clm01"><img src="https://colors.cc/line_test/intro001.jpg" alt="お知らせ" /></div>
         <div class="clm03">
-          <div><img src="https://colors.cc/line_test/nav001.jpg" alt="お知らせ" /></div>
-          <div><img src="https://colors.cc/line_test/nav002.jpg" alt="プロフィール" /></div>
-          <div><img src="https://colors.cc/line_test/nav003.jpg" alt="同窓会" /></div>
+          <div><Link to="/page1"><img src="https://colors.cc/line_test/nav001.jpg" alt="お知らせ" /></Link></div>
+          <div><Link to="/page2"><img src="https://colors.cc/line_test/nav002.jpg" alt="プロフィール" /></Link></div>
+          <div><Link to="/page3"><img src="https://colors.cc/line_test/nav003.jpg" alt="同窓会" /></Link></div>
           <div><img src="https://colors.cc/line_test/nav004.jpg" alt="コミュニティ" /></div>
           <div><img src="https://colors.cc/line_test/nav005.jpg" alt="お問い合わせ" /></div>
           <div><img src="https://colors.cc/line_test/nav006.jpg" alt="設定" /></div>
