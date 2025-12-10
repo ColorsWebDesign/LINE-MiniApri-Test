cb87c45f7eda9c6d9da2901d834fb108270048ce
ColorsWebDesign <fujiwara@recap.jp>
2025-12-10 07:32:19 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import { AppRoutes } from './Routes.jsx' // Routes.jsxを参照
+import Page1 from "./components/page1.jsx"; // pageA.jsxの読み込み
+import Page2 from "./components/page2.jsx"; // pageB.jsxの読み込み
+import Page3 from "./components/page3.jsx"; // pageB.jsxの読み込み
 import "./App.css";
 
 function App() {
@@ -34,11 +36,17 @@ function App() {
         <div class="clm01"><img src="https://colors.cc/line_test/intro001.jpg" alt="お知らせ" /></div>
         <div class="clm03">
           <div><Link to="/page1">Page1</Link></div>
-          <div><Link to="/page2">Page1</Link></div>
-          <div><Link to="/page3">Page1</Link></div>
+          <div><Link to="/page2">Page2</Link></div>
+          <div><Link to="/page3">Page3</Link></div>
           <div><img src="https://colors.cc/line_test/nav004.jpg" alt="コミュニティ" /></div>
           <div><img src="https://colors.cc/line_test/nav005.jpg" alt="お問い合わせ" /></div>
           <div><img src="https://colors.cc/line_test/nav006.jpg" alt="設定" /></div>
+
+       <Routes>
+           <Route path="/page1" element={<Page1 />} />
+           <Route path="/page2" element={<Page2 />} />
+           <Route path="/page3" element={<Page3 />} />
+       </Routes>
 
         </div>
       </div>
